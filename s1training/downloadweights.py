0bff3452a45a63bdcf3287066db4b197be33448f
hbzhang <hbzhang@vt.edu>
2025-03-22 12:51:15 -0500

    change

--- s1training/downloadweights.py
+++ s1training/downloadweights.py
@@ -1,12 +1,17 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-# Load the model and tokenizer
 model_name = "simplescaling/s1.1-32B"
 
 # Load tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-# Load the model
-model = AutoModelForCausalLM.from_pretrained(model_name)
+# Load model
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
 
-# You can now use the model for inference!
+print("Model downloaded successfully!")
+
+
+
+
+
+
